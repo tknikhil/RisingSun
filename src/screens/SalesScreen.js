6fab7a5e88b0adeac15d4dbf24e41cd73577d831
tknikhi <nikhiltk02@gmail.com>
2023-05-22 22:55:09 +0530

    dropdown done need to make generic

--- src/screens/SalesScreen.js
+++ src/screens/SalesScreen.js
@@ -1,21 +1,50 @@
 import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { SearchableDropDown } from '../app-widget'
+import React,{useRef, useState} from 'react'
 import ddown from "../../assets/json-request/ddown.json"
 import Icon from 'react-native-vector-icons/dist/AntDesign';
-import { TouchableOpacity } from 'react-native-gesture-handler'
+import { FlatList, TextInput, TouchableOpacity } from 'react-native-gesture-handler'
 
 const SalesScreen = () => {
- 
+ const [selectedCustomer,setSelectedCustomer]=useState('Select Customer');
+ const [isDropDownOpen,setIsDropDownOpen]=useState(false);
+ const [data,setData]=useState(ddown);
+ const searchRef=useRef();
+ const onSearch=(txt)=>{
+  if(txt !==''){
+    let tempData = data.filter(item=>{
+      return item.name.toLowerCase().match(txt.toLowerCase())
+    });
+    setData(tempData);
+  }else{
+    setData(ddown);
+  }
+ }
   return (
     
      <View style={styles.container}>
       <Text style={styles.header}>Customer</Text>
-      <TouchableOpacity style={styles.dropdownSelector}>
-        <Text>Select Customer</Text>
-        <Icon name="down" size={20}/> 
+      <TouchableOpacity style={styles.dropdownSelector} onPress={()=>{setIsDropDownOpen(!isDropDownOpen)}}>
+        <Text>{selectedCustomer}</Text>
+        {isDropDownOpen?<Icon name="up" size={20}/>:<Icon name="down" size={20}/>}
       </TouchableOpacity>
+     {isDropDownOpen? <View style={styles.dropdownArea}>
+      <TextInput ref={searchRef} placeholder='Search' style={styles.searchInput} onChangeText={(txt)=>{onSearch(txt);}}/>
+      <FlatList data={data} renderItem={({item,index})=>{
+        return(
+          <TouchableOpacity style={styles.ddownItems} onPress={()=>{
+            setSelectedCustomer(item.name);
+            onSearch('');
+            setIsDropDownOpen(false);
+            searchRef.current.clear();
+          }}>
+            <Text>{item.name}</Text>
+          </TouchableOpacity>
+        );
+      }}/>
+     </View>:null}
+     <View style='styles.box'>
+      <Text>Hi</Text>
+     </View>
     </View>
     
   )
@@ -24,7 +53,8 @@ const SalesScreen = () => {
 export default SalesScreen
 const styles=StyleSheet.create({
   container:{
-    flex:1
+    flex:1,
+    backgroundColor:"#f9fde9",
   },
   header:{
     fontSize:19,
@@ -38,7 +68,8 @@ dropdownSelector:{
   height:40,
   borderRadius:10,
   borderWidth:1,
-  borderColor:'#0000FF',
+  borderColor:'#6699cc',
+  backgroundColor:'#fff',
   marginTop:5,
   marginLeft:10,
   flexDirection:'row',
@@ -47,4 +78,42 @@ dropdownSelector:{
   paddingLeft:15,
   paddingRight:15
 },
+dropdownArea:{
+  width:'91%',
+  height:200,
+  borderBottomLeftRadius:10,
+  borderBottomRightRadius:10,
+  marginLeft:19,
+  // borderWidth:1,
+  // borderColor:'#6699cc',
+  backgroundColor:'#fff',
+  elevation:5,
+},
+searchInput:{
+  width:'100%',
+  height:40,
+  borderBottomLeftRadius:5,
+  borderBottomRightRadius:5,
+  borderEndWidth:1,
+  // borderColor:'#6699cc',
+  alignSelf:'center',
+  elevation:2
+},
+ddownItems:{
+  width:'95%',
+  height:50,
+  borderBottomWidth:.2,
+  alignSelf:'center',
+  borderBottomColor:'#8e8e8e',
+  justifyContent:'center'
+},
+box:{
+  width:'100%',
+  height:'70%',
+  marginTop:30,
+  borderWidth:1,
+  backgroundColor:'#fff',
+  padding:5,
+}
+
 });
