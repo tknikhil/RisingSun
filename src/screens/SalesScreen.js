5518d1178790432f15adf9416f483bad84460cac
tknikhi <nikhiltk02@gmail.com>
2023-06-26 04:39:56 +0530

    need to adujestment in salesscreen

--- src/screens/SalesScreen.js
+++ src/screens/SalesScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet ,Dimensions,Modal,TouchableOpacity, ScrollView,ActivityIndicator} from 'react-native'
+import { View, Text, StyleSheet ,Dimensions,Modal,TouchableOpacity, ScrollView,ActivityIndicator,Image,Animated} from 'react-native'
 import React,{useRef, useState,useEffect} from 'react'
 import ddown from "../../assets/json-request/ddown.json"
 import Icon from 'react-native-vector-icons/dist/AntDesign'
@@ -26,6 +26,31 @@ const SalesScreen = ({}) => {
 
  const [isPopupVisible, setIsPopupVisible] = useState(false);
  const [isCustomerSelected, setIsCustomerSelected] = useState(false);
+
+//#region menu
+ const [showMenu, setShowMenu] = useState(false);
+ const moveToRight = useRef(new Animated.Value(1)).current;
+ const scale = useRef(new Animated.Value(1)).current;
+ const [selectedMenuItem, setSelectedMenuItem] = useState(0);
+ const menu = [
+   {icon: require('../../assets/images/home.png'), title: 'Home'},
+   {icon: require('../../assets/images/product.png'), title: 'Products'},
+   {icon: require('../../assets/images/customer.png'), title: 'Customers'},
+   {icon: require('../../assets/images/logout.png'), title: 'Logout'},
+ ];
+ const renderSelectedScreen = () => {
+   switch (menu[selectedMenuItem].title) {
+     case 'Products':
+       return <ProductsScreen />; // Render the ProductsScreen component for the 'Products' menu item
+     case 'Customers':
+       return <CustomersScreen />; // Render the CustomersScreen component for the 'Customers' menu item
+     // Add cases for other menu items as needed
+     default:
+       return null;
+   }
+ };
+//#endRegion menu
+
 
   const openPopup = () => {
     setIsPopupVisible(true);
@@ -85,9 +110,11 @@ const fetchData = async () => {
 
   try {
     const response = await axios.get(
-      `http://192.168.0.244:9006/PosErp/v1/Act/customer?itemSize=5&page=${page}`
+      // `http://192.168.0.244:9006/PosErp/v1/Act/customer?itemSize=5&page=${page}`
+      `https://randomuser.me/api/?page=${page}&results=5`
     );
-    const newData = response.data.customerListInPage;
+    // const newData = response.data.customerListInPage;
+    const newData= response.data.results;
     console.log(newData);
 
     setCData((prevData) => [...prevData, ...newData]);
@@ -112,8 +139,16 @@ const handleLoadMore = () => {
   console.log('handleCustomer');
  }
   return (
-   
-     <View style={styles.container}>
+      <View style={styles.container}>
+        {/* Menu */}
+        <LinearGradient
+      colors={['#9DC5C3', '#5E5C5C']}>
+      <View >
+        {/* Menu Design */}
+        {avatar()}
+        {menuItem()}
+        {drawer()}
+            {/* DropDown */}
       <View style={styles.dropdownContainer}>
       <Text style={styles.header}>Customer</Text>
       <View style={styles.rowDropdownContainer}>
@@ -356,6 +391,9 @@ const handleLoadMore = () => {
     <View style={styles.submitBtnContainer}>
     <Button onPress={handleSubmit} text={'Submit'} style={styles.newbutton} />
     </View>
+      </View>
+    </LinearGradient>
+
     </View>
 
     
@@ -363,6 +401,144 @@ const handleLoadMore = () => {
     
     
   )
+
+  function avatar() {
+    return (
+      <View
+        style={{
+          width: '100%',
+          flexDirection: 'row',
+          alignItems: 'center',
+          marginTop: 30,
+        }}>
+        <Image
+          source={require('../../assets/images/person.png')}
+          style={{
+            width: 70,
+            height: 70,
+            borderRadius: 35,
+            marginLeft: 20,
+            backgroundColor: 'white',
+            borderWidth: 3,
+            borderColor: 'grey',
+          }}
+        />
+        <View style={{marginLeft: 10}}>
+          <Text style={{fontSize: 20, fontWeight: 'bold', color: 'white'}}>
+            Nidhish TK
+          </Text>
+          <Text style={{fontSize: 15, color: 'white'}}>Sales</Text>
+        </View>
+      </View>
+    );
+  }
+
+  function menuItem() {
+    return (
+      <View style={{marginTop: 30}}>
+        <FlatList
+          data={menu}
+          renderItem={({item, index}) => {
+            return (
+              <TouchableOpacity
+                style={{
+                  width: 200,
+                  // height: 10,
+                  marginLeft: 20,
+                  marginTop: 20,
+                  backgroundColor: selectedMenuItem === index ? '#fff' : null,
+                  borderRadius: 10,
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  setSelectedMenuItem(index);
+
+                  //  navigation.navigate(menu[selectedMenuItem].title);
+                  console.log(item.title);
+                  // console.log(setSelectedMenuItem(index));
+                }}>
+                <Image
+                  source={item.icon}
+                  style={{
+                    width: 24,
+                    height: 24,
+                    marginLeft: 10,
+                    tintColor: selectedMenuItem === index ? '#000' : '#fff',
+                  }}
+                />
+                <Text
+                  style={{
+                    fontSize: 18,
+                    fontWeight: '800',
+                    marginLeft: 15,
+                    color: selectedMenuItem === index ? '#000' : '#fff',
+                  }}>
+                  {item.title}
+                </Text>
+              </TouchableOpacity>
+            );
+          }}
+        />
+      </View>
+    );
+  }
+
+  function drawer() {
+    return (
+      <Animated.View
+        style={{
+          flex: 1,
+          backgroundColor: '#f5f5f5',
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          bottom: 0,
+          top: 0,
+          transform: [{scale: scale}, {translateX: moveToRight}],
+          borderRadius: showMenu ? 15 : 0,
+        }}>
+        <View style={{flexDirection: 'row', marginTop: 30}}>
+          <TouchableOpacity
+            style={{marginLeft: 20}}
+            onPress={() => {
+              Animated.timing(scale, {
+                toValue: showMenu ? 1 : 0.8,
+                duration: 200,
+                useNativeDriver: true,
+              }).start();
+              Animated.timing(moveToRight, {
+                toValue: showMenu ? 0 : 230,
+                duration: 200,
+                useNativeDriver: true,
+              }).start();
+              setShowMenu(!showMenu);
+            }}>
+            <Image
+              source={require('../../assets/images/menu.png')}
+              style={{width: 30, height: 30}}
+            />
+          </TouchableOpacity>
+          <Text style={{marginLeft: 20, fontSize: 20, fontWeight: '800'}}>
+            {menu[selectedMenuItem].title}
+          </Text>
+        </View>
+        {screenCall()}
+      </Animated.View>
+    );
+    function screenCall() {
+      return (
+        <View
+          style={{
+            width: '100%',
+            height: '100%',
+            borderBottomLeftRadius: showMenu ? 15 : 0,
+          }}>
+          {renderSelectedScreen()}
+        </View>
+      );
+    }
+  }
 }
 
 export default SalesScreen
--- src/url/ConstantURL.js
+++ src/url/ConstantURL.js
@@ -4,6 +4,7 @@ import React from 'react'
 // const ConstantURL = () => {
  export const BASE_URL=()=>'http://192.168.0.244:9006/PosErp/v1';
  export const LOGIN_URL=()=>'/Auth/login';
+ export const PAGINATION=()=>'https://randomuser.me/api/?'
 // }
 
 // export default ConstantURL
